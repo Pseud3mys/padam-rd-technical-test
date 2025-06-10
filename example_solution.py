4d4107c2d981c3ee1b46d40fff56727612c0c586
AlbanPadam <alban.guerbois@padam.io>
2025-06-10 16:53:27 +0200

    Add incomplete example solution

---
+++ example_solution.py
@@ -0,0 +1,24 @@
+from graph import Graph
+
+
+def example_solution(graph: Graph) -> list:
+    """Implement a naive solution to find a path in the graph which travels all edges."""
+    uncovered_edges = set(graph.edges)
+    current_vertex = 0  # Pick a starting vertex
+    path = []
+
+    while uncovered_edges:
+        next_vertex=None
+        for edge in uncovered_edges:
+            if current_vertex in edge[:2]:
+                next_vertex = edge[1] if edge[0] == current_vertex else edge[0]
+                
+        if next_vertex:
+            path.append(edge)
+            uncovered_edges.remove(edge)
+            current_vertex = next_vertex
+        else:
+            print("Problem detected")
+            break
+    
+    return path
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from input import parse_cmd_line, parse_file
 from graph import Graph
+from example_solution import example_solution
 
 
 def main():
@@ -7,6 +8,12 @@ def main():
     vertices, edges = parse_file(in_file)
     print(f"#E={len(edges)}, #V={len(vertices)}")
     graph = Graph(vertices, edges)
+
+    path = example_solution(graph)
+
+    print("Length of path found:", len(path) + 1)
+    print("Value of path found:", sum(edge[2] for edge in path))
+
     if plot_graph:
         graph.plot()
 
